0822ada51dbcc9eff24beab950075562bffb753a
joaquingutierrez <joaquinguty@gmail.com>
2022-12-19 19:34:59 -0300

    Agregado fs para la persistencia de los productos

--- ProductManager.js
+++ ProductManager.js
@@ -1,50 +1,74 @@
+const fs = require('fs')
+
 class ProductManager {
     static id = 0;
-    constructor() {
-        this.products = [];
+    constructor(path) {
+        this.path = path;
+        this.getProducts()
+        this.file = fs.writeFileSync(this.path, JSON.stringify(this.products))
     }
     addProduct(product) {
         const { title, description, price, thumbnail, code, stock } = product;
+        this.getProducts();
+        const lastIndex = this.products.length - 1
+        if (lastIndex >= 0) {
+            ProductManager.id = this.products[lastIndex].id
+        }
         if (title && description && price && thumbnail && code && stock) {
             if (!this.products.some((item) => item.code === product.code)) {
                 ProductManager.id++;
-                return this.products = [
-                    ...this.products,
-                    {
-                        ...product,
-                        id: ProductManager.id
-                    }
-                ]
+                product = {
+                    ...product,
+                    id: ProductManager.id
+                };
+                this.products.push(product)
+                return this.writeProductsList();
+
             }
-            throw new Error('The product already exists in the list')
+            throw new Error('The product already exists in the list');
         }
-        throw new Error("Error: Missing product's properties")
+        throw new Error("Error: Missing product's properties");
     }
     getProducts() {
-        return this.products
+        try {
+            return this.products = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+        }
+        catch {
+            return this.products = []
+        }
+    }
+    writeProductsList() {
+        fs.writeFileSync(this.path, JSON.stringify(this.products, '', '\t'));
     }
     getProductById(id) {
-        const productFiltered = (this.products.filter((item) => item.id === id))
+        this.getProducts();
+        const productFiltered = (this.products.find((item) => item.id === id))
         if (productFiltered) {
             return productFiltered
         }
         throw new Error('Error: Product not found')
     }
-    updateProduct(id, product) {
-
+    updateProduct(id, property, value) {
+        this.getProducts();
+        const productId = this.getProductById(id);
+        Object.defineProperty(productId, property, { value: value });
+        this.writeProductsList();
     }
     deleteProduct(id) {
-
+        const productId = this.getProductById(id);
+        const index = this.products.indexOf(productId);
+        this.products.splice(index, 1);
+        this.writeProductsList();
     }
 }
 
 
 //test
 
-const testList = new ProductManager()
+// const testList = new ProductManager('./testList.JSON')
 
-console.log(ProductManager.id)
-console.log(testList.getProducts())
+// console.log(ProductManager.id)
+// console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
@@ -94,34 +118,38 @@ const test5 = {
 }
 
 
-console.log(ProductManager.id)
-testList.addProduct(test)
-console.log(testList.getProducts())
+//console.log(ProductManager.id)
+// testList.addProduct(test)
+//console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
-console.log(ProductManager.id)
-//testList.addProduct(test2)
-console.log(testList.getProducts())
+//console.log(ProductManager.id)
+// testList.addProduct(test2)
+//console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
-console.log(ProductManager.id)
-testList.addProduct(test3)
-console.log(testList.getProducts())
+//console.log(ProductManager.id)
+// testList.addProduct(test3)
+//console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
-console.log(ProductManager.id)
-testList.addProduct(test4)
-console.log(testList.getProducts())
+//console.log(ProductManager.id)
+// testList.addProduct(test4)
+//console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
-console.log(ProductManager.id)
-//testList.addProduct(test5)
-console.log(testList.getProducts())
+//console.log(ProductManager.id)
+// testList.addProduct(test5)
+//console.log(testList.getProducts())
 
 console.log('-------------------------------------')
 
-console.log(testList.getProductById(1));
+//console.log(testList.getProductById(1));
+
+// testList.updateProduct(2, 'price', 999999999)
+
+// testList.deleteProduct(2)
