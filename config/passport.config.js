fbdcc84443c24532b17ab6e28ede922a2f37c444
joaquingutierrez <joaquinguty@gmail.com>
2023-03-07 15:52:50 -0300

    passport.config hecho

---
+++ config/passport.config.js
@@ -0,0 +1,56 @@
+const passport = require("passport")
+const local = require("passport-local")
+const { userModel } = require('../dao/mongoManager/models/users.model');
+const { passwordHash, isValidPassword } = require("../utils")
+
+const LocalStrategy = local.Strategy
+const initializePassport = () => {
+
+    passport.use("register", new LocalStrategy(
+        { passReqToCallback: true, usernameField: "email" }, async (req, username, password, done) => {
+            const { first_name, last_name, age, email } = req.body
+            try {
+                let user = await userModel.findOne({ email: username })
+                if (user) {
+                    console.log("User already exists")
+                    return done(null, false)
+                }
+                const newUser = {
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password: passwordHash(password)
+                }
+                let result = await userModel.create(newUser)
+                return done(null, result)
+            }
+            catch (err) {
+                return done("Error al intentar obtener al usuario: ", err)
+
+            }
+        }
+    ))
+
+    passport.use("login", new LocalStrategy(
+        { usernameField: "email" }, async (username, password, done) => {
+            try {
+                const user = await userModel.findOne({ email: username })
+                if (!user) {
+                    console.log("User doesn't exist")
+                    return done(null, false)
+                }
+                if (!(await isValidPassword(password, user))) return done(null, false)
+                return done(null, user)
+            }
+            catch (err) {
+                return done(err)
+            }
+        }
+    ))
+
+    module.exports = {
+        initializePassport
+    }
+
+}
