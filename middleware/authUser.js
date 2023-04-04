2132807e2ba794fbb52d4ebaa47efe940eae6abf
joaquingutierrez <joaquinguty@gmail.com>
2023-04-04 14:41:29 -0300

    Autentificacion para que solo los usuarios puedan acceder al chat

---
+++ middleware/authUser.js
@@ -0,0 +1,14 @@
+const authUser = (req, res, next) => {
+    const user = req.session.user || "none"
+    if (user.rol !== "ADMIN") {
+        console.log("autorizado")
+        next()
+    } else {
+        console.log("Usuario no autorizado")
+        res.send("Usuario no autorizado")
+    }
+}
+
+module.exports = {
+    authUser
+}
--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const chatRouter = express.Router();
-const {renderChat} = require("../controller/chat.controller")
+const { renderChat } = require("../controller/chat.controller")
+const {authUser} = require("../middleware/authUser")
 
-
-chatRouter.get('', renderChat)
+chatRouter.get('', authUser, renderChat)
 
 module.exports = {
     chatRouter
