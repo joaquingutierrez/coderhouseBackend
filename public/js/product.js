e13ec62e54ead8f0b818e95d85219edb65a0125d
joaquingutierrez <joaquinguty@gmail.com>
2023-04-04 14:30:29 -0300

    Ahora el admin puede actualizar cualquier producto

--- public/js/product.js
+++ public/js/product.js
@@ -17,3 +17,37 @@ deleteProduct.addEventListener("click", () => {
     })
 })
 
+const updateProduct = document.getElementById("updateProduct")
+updateProduct.addEventListener("click", (e) => {
+    e.preventDefault()
+    const title = document.getElementById("title").value
+    const description = document.getElementById("description").value
+    const code = document.getElementById("code").value
+    const price = document.getElementById("price").value
+    const stock = document.getElementById("stock").value
+    const category = document.getElementById("category").value
+    const updatedProduct = {
+        title: title || undefined,
+        description: description || undefined,
+        code: code || undefined,
+        price: price || undefined,
+        stock: stock || undefined,
+        category: category || undefined
+    }
+    fetch("/api/products/" + productId, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            updatedProduct
+        )
+    })
+    .then(res => res.text())
+    .then(data => {
+        if (data === "Usuario no autorizado") {
+            return alert("Usuario no autorizado para actualizar el producto")
+        }
+        return window.location.href = "/api/products/" + productId
+    })
+})
