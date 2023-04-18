8926a28720acd527e5bc9872fabebbec743412c7
joaquingutierrez <joaquinguty@gmail.com>
2023-04-18 11:40:12 -0300

    Cambios al modelo de productos para que los campos sean requeridos

--- dao/mongoManager/models/products.model.js
+++ dao/mongoManager/models/products.model.js
@@ -4,17 +4,38 @@ const mongoosePaginate = require("mongoose-paginate-v2")
 const productsCollection = "products"
 
 const productsSchema = new mongoose.Schema({
-    title: String,
-    description: String,
+    title: {
+        type:String,
+        require:true
+    },
+    description: {
+        type:String,
+        require:true
+    },
     code: {
         type: String,
         unique: true
     },
-    price: Number,
-    thumbnail: Array,
-    stock: Number,
-    category: String,
-    status: Boolean
+    price: {
+        type:Number,
+        require:true
+    },
+    thumbnail: {
+        type: Array,
+        require:true
+    },
+    stock: {
+        type: Number,
+        require:true
+    },
+    category: {
+        type:String,
+        require:true
+    },
+    status: {
+        type:String,
+        default:true
+    }
 })
 productsSchema.plugin(mongoosePaginate)
 
