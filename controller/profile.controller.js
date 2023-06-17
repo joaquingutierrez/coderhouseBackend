5f954f893b09935f8063f40e8cbd56b9d3dfee44
joaquingutierrez <joaquinguty@gmail.com>
2023-06-17 17:37:24 -0300

    Estilos para profile

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -2,7 +2,7 @@ const { UserDto } = require("../dao/dto/user.dto")
 
 const profile = function (req, res) {
     const user = new UserDto(req.session.user)
-    res.render("profile", { user })
+    res.render("profile", {style: "/css/profile.css", user })
 }
 const getProfile = function (req, res) {
     const user = new UserDto(req.session.user)
