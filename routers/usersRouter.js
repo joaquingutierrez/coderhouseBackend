a11617a796b33bddfc98579b3bdc6a22127317ea
joaquingutierrez <joaquinguty@gmail.com>
2023-06-17 19:39:57 -0300

    Ahora para la ver la lista de usuarios y borrarlos solo se puede hacer si se tiene rango ADMIN

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const usersRouter = express.Router();
 const { changeRol, uploadDocuments, getAllUsers, deleteInactiveUsers, deleteUserByEmail } = require("../controller/users.controller")
 const { uploader } = require("../middleware/multer")
+const {auth} = require("../middleware/auth")
 
 const multerMiddleware = uploader.single('uploaded_file')
 
-usersRouter.get("", getAllUsers)
-usersRouter.delete("", deleteInactiveUsers)
-usersRouter.delete("/:email", deleteUserByEmail)
+usersRouter.get("",auth, getAllUsers)
+usersRouter.delete("", auth, deleteInactiveUsers)
+usersRouter.delete("/:email", auth, deleteUserByEmail)
 usersRouter.get("/premium/:uid", changeRol)
 usersRouter.post("/:uid/documents", multerMiddleware, uploadDocuments)
 
